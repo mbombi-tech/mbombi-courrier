ea560f0d6a053bd3128be812b2b9d6effb59878b
mbombi-tech <kingmbombi1@Gmail.com>
2026-02-26 00:31:28 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,6 @@
+
 <?php
+// db.php — Connexion MySQL avec PDO
 
 $host = "gateway01.eu-central-1.prod.aws.tidbcloud.com";
 $port = 4000;
@@ -7,18 +9,12 @@ $user = "3CcRhwJGS9Zx9di.root";
 $pass = "agj9cdG5scfUtrzC";
 
 try {
+    // DSN = Data Source Name
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
 
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
-
-    $options = [
-        PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_PERSISTENT => false
-    ];
-
-    $db = new PDO($dsn, $user, $pass, $options);
+    // Option pour les erreurs PDO
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-    die("Erreur connexion base de données : " . $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
-?>
