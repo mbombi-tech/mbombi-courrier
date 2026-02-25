a77002cc2ad4cd5de9b0f2f8fc3ac8b3794df87a
mbombi-tech <kingmbombi1@Gmail.com>
2026-02-25 15:19:12 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . "/courrier/conn.php";
+require_once __DIR__ . "/conn.php";
 
 // 🔁 Si déjà connecté → dashboard
 if (isset($_SESSION['user']['id'])) {
