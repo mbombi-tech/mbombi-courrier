b3ae480d4b5493bc01d810564eca87aa1f675fa6
mbombi-tech <kingmbombi1@Gmail.com>
2026-02-25 16:43:58 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,25 +3,22 @@
 $host = "gateway01.eu-central-1.prod.aws.tidbcloud.com";
 $port = 4000;
 $dbname = "dbcourriers";
-$user = "3CcRhwJGS9Zx9di.root";   // ⚠️ prends le username EXACT TiDB
+$user = "mbombi-tech";
 $pass = "TON_MOT_DE_PASSE_TIDB";
 
 try {
 
-    $db = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt'
-        ]
-    );
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => false
+    ];
+
+    $db = new PDO($dsn, $user, $pass, $options);
 
 } catch (PDOException $e) {
-
     die("Erreur connexion base de données : " . $e->getMessage());
-
 }
+?>
