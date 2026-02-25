f8bc58966d4226c9267da7b3f1a55be931f95a5e
mbombi-tech <kingmbombi1@Gmail.com>
2026-02-25 16:35:05 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,24 +1,27 @@
 <?php
-// Connexion TiDB Cloud avec PDO
 
 $host = "gateway01.eu-central-1.prod.aws.tidbcloud.com";
 $port = 4000;
 $dbname = "dbcourriers";
-$user = "mbombi-tech";
-$pass = "Richessen10?";
+$user = "3CcRhwJGS9Zx9di.root";   // ⚠️ prends le username EXACT TiDB
+$pass = "TON_MOT_DE_PASSE_TIDB";
 
 try {
 
     $db = new PDO(
         "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
         $user,
-        $pass
+        $pass,
+        [
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt'
+        ]
     );
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-    die("Erreur connexion base de données : " . $e->getMessage());
-}
 
-?>
+    die("Erreur connexion base de données : " . $e->getMessage());
+
+}
